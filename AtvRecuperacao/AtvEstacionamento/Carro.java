2b742361d687817c82fe3adc137838852fed6384
Meguelzz <meguelzz.oliveira@gmail.com>
2025-04-03 20:43:03 -0300

    Delete AtvRecuperacao directory

--- AtvRecuperacao/AtvEstacionamento/Carro.java
+++
@@ -1,8 +0,0 @@
-public class Carro extends Veiculo {
-    int NumeroPortas;
-
-    Carro(String Modelo, String Placa, int Ano) {
-        super(Modelo, Placa, Ano);
-        this.NumeroPortas = NumeroPortas;
-    }
-}
--- AtvRecuperacao/AtvEstacionamento/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String [] args) {
-
-    Veiculo veiculo = new Veiculo ("Caloi", "123", 2008);
-
-    veiculo.exibirDetalhes();
-    }
-}
