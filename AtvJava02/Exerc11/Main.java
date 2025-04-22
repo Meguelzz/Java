f610a16a2013a286738388da10e230ec928b048e
Meguelzz <meguelzz.oliveira@gmail.com>
2025-04-22 20:48:00 -0300

    Add files via upload

---
+++ AtvJava02/Exerc11/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+         a1.setNome("Miguel");
+         a1.setNota1(8.0);
+         a1.setNota2(7.0);
+
+         Aluno a2 = new Aluno();
+         a2.setNome("Gustavo");
+         a2.setNota1(3.0);
+         a2.setNota2(4.0);
+    }
+}
