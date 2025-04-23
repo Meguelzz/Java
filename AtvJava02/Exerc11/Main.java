2b1659a57a69fe1cd6ca4178110c5be10a88fee8
Meguelzz <meguelzz.oliveira@gmail.com>
2025-04-22 21:28:20 -0300

    Update Main.java

--- AtvJava02/Exerc11/Main.java
+++ AtvJava02/Exerc11/Main.java
@@ -3,11 +3,15 @@ public class Main {
         Aluno a1 = new Aluno();
          a1.setNome("Miguel");
          a1.setNota1(8.0);
-         a1.setNota2(7.0);
+         a1.setNota2(10.00);
+         a1.calcularMedia(0, 0);
+         a1.estaAprovado();
 
          Aluno a2 = new Aluno();
          a2.setNome("Gustavo");
          a2.setNota1(3.0);
          a2.setNota2(4.0);
+         a2.calcularMedia(0, 0);
+         a2.estaAprovado();
     }
 }
