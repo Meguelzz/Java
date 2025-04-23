877df39cebe82f1762be8a8b827c3ddc037725ad
Meguelzz <meguelzz.oliveira@gmail.com>
2025-04-23 20:38:56 -0300

    Delete AtvJava02 directory

--- AtvJava02/br/edu/etec/exerc6e7/model/ClasseA.java
+++
@@ -1,5 +0,0 @@
-package br.edu.etec.exerc6e7.model;
-
-public class ClasseA {
-    int numero = 10;
-}
--- AtvJava02/br/edu/etec/exerc6e7/model/ClasseB.java
+++
@@ -1,8 +0,0 @@
-package br.edu.etec.exerc6e7.model;
-
-public class ClasseB {
-    public void testarAcesso() {
-        ClasseA objA = new ClasseA();
-        System.out.println(objA.numero);
-    }
-}
